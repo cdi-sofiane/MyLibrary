1204c9d6d7efca6162559cd9bfaf00e6b922167e
cdi-sofiane <42858371+cdi-sofiane@users.noreply.github.com>
2021-01-23 19:36:18 +0100

    remover comments

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-
 import { } from "./src/config/env.js";
 import express from "express";
-import connection from "./src/route/connection.js";
 import dashboard from "./src/route/dashbord.js";
 import session from "express-session";
 
-import nameResolver from './src/helper/nameResolver.js';
 
 const { host, port, secret } = process.env;
 //library
@@ -20,11 +17,7 @@ app.use(session({
     cookie: { expires: 3600 * 60 * 1 * 100 },
 
 }))
-// var k = [];
-// app.use('/', nameResolver, (req, res, next) => { var k = "roro" })
 
-// console.log(k)
-// app.use(Promise.resolve(nameResolver), Promise.resolve(nameResolver));
 app.use('/dashboard', dashboard);
 app.listen(port, () => {
     console.log(`serveur lancer sur l\'addresse ${host + port}`);
